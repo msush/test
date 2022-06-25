07a32e1ee6e1399082f2c5e798751cf2dcc3ba7b
msush <mulukutla.chaitanya@gmail.com>
2022-06-25 12:47:29 -0400

    terraform s3

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,38 +1,17 @@
-resource "aws_instance" "web" {
-    ami = "ami-0cff7528ff583bf9a"
-    instance_type="t2.micro"
-    key_name = "trail1"
-    tags={
-        Name="master"
-    }
-    
+variable "region" {
+    default = "us-east-1"
 }
-
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-
-  dynamic "ingress" {
-        for_each = var.web_ingress
-        content {
-            description = "Ingress "
-            from_port = ingress.value.port
-            to_port = ingress.value.port
-            protocol = ingress.value.protocol
-            cidr_blocks = ingress.value.cidr_blocks
+variable "web_ingress" {
+    type = map(object({
+        port = number
+        protocol = string
+        cidr_blocks = list(string)
+    }))
+    default = {
+        "80" = {
+            port = 80
+            protocol = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
         }
     }
-  
-  
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
 }
