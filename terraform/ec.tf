cf25d5eb35a6ffe80c4e8767e601c662c144a519
msush <mulukutla.chaitanya@gmail.com>
2022-06-25 13:18:15 -0400

    added master slave and ansible nodes

--- terraform/ec.tf
+++ terraform/ec.tf
@@ -2,8 +2,9 @@ resource "aws_instance" "web" {
     ami = "ami-0cff7528ff583bf9a"
     instance_type="t2.micro"
     key_name = "trail1"
+    for_each = toset(["master", "slave", "ansible_host"])
     tags={
-        Name="master"
+        Name="${each.key}"
     }
     
 }
